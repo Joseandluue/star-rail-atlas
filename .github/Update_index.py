a2277641c4e45a56016b41486cb2860c21eb812d
Joseandluue <joseluue@outlook.com>
2024-04-22 18:57:15 +0800

    Update Update_index.py

--- .github/Update_index.py
+++ .github/Update_index.py
@@ -8,11 +8,8 @@ root_path = Path(__file__).parent.parent
 file = ['files.json','othername.json','path.json']
 
 for file_name in file:
-    print(file_name)
     res = requests.get(f'https://raw.githubusercontent.com/Nwflower/star-rail-atlas/master/{file_name}')
-    # print(res.text)
     new_fileHash = hashlib.md5(res.content).hexdigest()
-    print(new_fileHash)
     old_filePath = root_path / file_name
     if not old_filePath.exists():
         with open(old_filePath, 'w', encoding='utf-8') as f:
@@ -26,3 +23,4 @@ for file_name in file:
             with open(old_filePath, 'w', encoding='utf-8') as f:
                 new_fileData = json.loads(res.text)
                 json.dump(new_fileData, f, ensure_ascii=False, indent=4)
+            print(f'已更新{file_name}')
